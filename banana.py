6e7130969bd6d2b4cf1287c182ee35c6d280759d
Peter Zhokhov <peterz@openai.com>
2018-12-30 12:52:32 -0800

    parsing and printing

--- banana.py
+++ banana.py
@@ -34,9 +34,42 @@ def _make_trial_crossword(letter_usage):
 def valid_crossword(crossword):
     raise NotImplementedError
 
-def print_crossword():
-    raise NotImplementedError
 
+def parse_crossword(crossword):
+    inserted_words = [] 
+    for word, idx, intersecting_word_idx, intersecting_letter_idx in crossword:
+        if idx is None:
+            x, y, horizontal = 0, 0, True # first word starts at 0, 0 (we will adjust that later if necessary and is horizontal)
+        else:
+            _, isct_x, isct_y, isct_horizontal = inserted_words[idx]
+            if isct_horizontal:
+                horizontal = False
+                x = isct_x + intersecting_letter_idx
+                y = isct_y - idx
+            else:
+                horizontal = True
+                x = isct_x - idx
+                y = isct_y + intersecting_letter_idx
+            
+        inserted_words.append((word, x, y, horizontal)) 
+
+
+def print_crossword(crossword, stream=sys.stdout):
+    max_size = 100
+    buffer = [[' '] * max_size] * max_size
+
+    horizontal = True
+    word_x, word_y = max_size // 2, max_size // 2
+    for word, x, y, horizontal in parse_crossword(crossword):
+        for idx, letter in enumerate(word):
+            if horizontal:
+                buffer[x + idx][y] = letter
+            else:
+                buffer[x][y + idx] = letter 
+    
+    for inner_buffer in buffer:
+        stream.write(''.join(inner_buffer))
+    
 
 def choose_intersection(crossword, letter_usage):
     raise NotImplementedError
